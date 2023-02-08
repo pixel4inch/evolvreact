29afddf628e391deddecbcc60100fdc2586b7adc
bojjapusrinivas <77658911+pixel4inch@users.noreply.github.com>
2023-02-08 18:33:18 +0530

    updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,13 +7,13 @@ import Profilebg from '../img/hero_page/profile.jpg'
 function Profile() {
   return (
     <>
-        <section class="section hero_section profile" style={{ backgroundImage: `url(${Profilebg})` }}>
-            <div class="container">
+        <section className="section hero_section profile" style={{ backgroundImage: `url(${Profilebg})` }}>
+            <div className="container">
         <h1>Evolv™ is a Silicon Valley based program of the Confederation of Indian Industry (CII), the non-profit
             powerhouse with <span>62</span> offices, <span>10</span> centers of excellence, <span>9000</span> corporate members, <span>11,000+</span>
             startups/scale-ups, and <span>300,000+</span>  affiliated entities worldwide. </h1>
             <br/>
-        <p class="text-left para-display4" >Evolv™ aspires to be the global facilitator
+        <p className="text-left para-display4" >Evolv™ aspires to be the global facilitator
             for innovators and their startups by connecting them to investors, big enterprise, and government through a
             myriad of events and engagements. Evolv™ provides the right platform for multidimensional business &amp;
             technology partnerships, thereby presenting the perfect global playbook. CII’s strategic objective is to
@@ -21,7 +21,7 @@ function Profile() {
             technological leadership, economic growth, and long-term nation building.</p>
             <br/>
 
-        <p class="text-left para-display4">As the official Secretariat for 2023 <a href="https://www.b20india2023.org/" class="linkblue" target="_blank">B20 Summit</a> and multiple programs it offers, this is a perfect
+        <p className="text-left para-display4">As the official Secretariat for 2023 <a href="https://www.b20india2023.org/" className="linkblue" target="_blank">B20 Summit</a> and multiple programs it offers, this is a perfect
             opportunity for innovators, investors, big enterprises, and governments to get engaged.</p>
         </div>
         </section>
