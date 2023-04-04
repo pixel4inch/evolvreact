c2478a1745dbcec86e7eb31ef4cc311ebb1bfb28
bojjapusrinivas <77658911+pixel4inch@users.noreply.github.com>
2023-04-04 18:22:46 +0530

    service agrment

--- src/pages/ServiceAggrement.jsx
+++ src/pages/ServiceAggrement.jsx
@@ -1,8 +1,280 @@
 import React from 'react'
 
+//import component 
+import { PageTitle } from '../components'
+
 function ServiceAggrement() {
   return (
-    <div>ServiceAggrement</div>
+    <>
+    <section className="design-team section pt-2">
+      <div className="container">
+       <PageTitle title="EVOLV™ SERVICE AGREEMENT" />
+           
+       <div class="col-xl-10 col-lg-12 col-12 mx-auto">
+
+            <p className="para-display2 d-block">Version: 1/2023 </p>
+            
+
+            <p className="para-display2 font-weight-bold  d-block  mt-3">GENERAL TERMS AND CONDITIONS </p>
+
+
+
+            <h4 class="font-display3 d-block mt-3">
+                1. INTRODUCTION AND BACKGROUND
+            </h4>
+
+
+
+
+            <p className=" para-display2 mb-3">CII-Evolv™ and its affiliate entities including data providers StrategyR™ and ZING™,
+                (hereinafter "Evolv™" or "we"), is an Indian-owned non-profit powerhouse, Confederation of Indian
+                Industry (hereinafter called “CII™”). These Evolv™ General Terms and Conditions (hereinafter the
+                "Terms") set out the terms and conditions applicable to and in connection with the use of Evolv™
+                Services and content. Accordingly, in these Terms, “Evolv™ Services” refer to but not limited to
+                Evolv™ conference, and online events. Datagraphics, content on markets and dashboard data presented
+                on Evolv™ website is licensed content from StrategyR™ and ZING™. Opt-in enrichments provided by
+                participants will be owned by StrategyR™ and ZING™. You may not share, distribute, or reproduce in
+                any way any copyrighted material, trademarks, or other proprietary information belonging to
+                StrategyR™ and ZING™ without obtaining the prior written consent of the owner of such proprietary
+                rights. We kindly ask you to read these Terms carefully and note that Evolv™ may, from time to time,
+                amend these Terms. </p>
+
+
+
+            <h4 class="font-display4">
+                2. USE OF EVOLV™ SERVICES
+            </h4>
+
+
+            <p className=" para-display2 mb-3"><u>2.1 Users:</u> : Evolv™ Services can be used by an individual user (“Individual
+                User”) or by an authorized representative of a legal entity (“Business User”). The Business User
+                confirms that the legal entity accepts these Terms and that the Business User has the right to
+                represent such entity for the purposes of these Terms. Individual User and the Business User
+                hereinafter together referred to as the "User", or “you”. By using and/or registering to Evolv™
+                Services, the User accepts and agrees to comply with and be bound by these Terms. </p>
+
+                <p className=" para-display2 mb-3"><u>2.2 User Accounts:</u> The User may register for Evolv™ Services by filling in
+                the registration form and creating a profile (hereinafter the “User Account”) on Evolv™ Services.
+                When registering to Evolv™ Services, the User is responsible for providing accurate and complete
+                registration data and other information as requested by Evolv™. The User may later modify the
+                information provided at the registration phase via personal User Account settings. Subject to a due
+                registration to Evolv™ Services and compliance with these Terms, Evolv™ grants the User a limited,
+                non-exclusive, non-transferable, and non-sublicensable right to use Evolv™ Services as offered to
+                you by Evolv™ from time to time for the purposes set out herein. Such granted right shall be valid
+                for as long as the User uses Evolv™ Services in accordance with these Terms. Users shall use all
+                reasonable care and endeavors to prevent unauthorized access to, or use of, the User Account. In the
+                event of, or if the User has justified reasons to suspect any unauthorized access or use of the User
+                Account, or if the User’s password has been disclosed to a third party, the User shall promptly
+                notify Evolv™ thereof. Evolv™ reserves the right to terminate and remove a User Account at any time,
+                for example, in case Evolv™ deems that the User has breached these Terms, added misleading,
+                inaccurate, inappropriate, unauthorized or illegal content to Evolv™ Services or caused harm to us
+                or the other Users of Evolv™ Services.</p>
+
+                <p className=" para-display2 mb-3"><u>2.3. Use Restrictions:</u> The User having a User Account is not permitted nor
+                entitled to do any of the following: </p>
+
+
+            <ul className="mt-3 pl-5">
+                <li>Sign up for a User Account on behalf of someone else; </li>
+                <li>Use the User Account for transmitting any unauthorized advertising, promotional materials,
+                    junk mail, spam, chain letters, contests, pyramid schemes, or any other form of solicitation
+                    or mass messaging; </li>
+                <li>Use the User Account in violation of applicable laws; </li>
+                <li>Use the User Account in ways that violate intellectual property rights, confidential
+                    information, business secrets, trade secrets or privacy of third parties; </li>
+                <li>Use the User Account to transmit any material that contains adware, malware, spyware,
+                    software viruses, worms or any other computer code designed to interrupt, destroy, or limit
+                    the functionality of computer software or equipment; </li>
+                <li>Any other similar use or action as mentioned above. </li>
+
+            </ul>
+
+
+            <h4 class="font-display4">
+                3. USER CONTENT ON EVOLV™ SERVICES
+            </h4>
+
+
+
+
+            <p className=" para-display2 mb-3">In connection with the registration and use of Evolv™ Services, Users may submit content, data and
+                information, including without limitation text, photographs and information through the chat
+                function, to Evolv™ Services (“User Content”). The User is solely responsible for all content that
+                the User uploads/transmits to Evolv™ Services, including ensuring that it has all relevant rights to
+                the User Content. In case Evolv™ reasonably deems that any User Content violates these Terms,
+                intellectual property rights or applicable law, Evolv™ reserves the right to remove such User
+                Content at any time and at its own discretion. Accordingly, the User agrees and acknowledges that
+                Evolv™ does not assume any liability or responsibility in respect to any User Content, and the User
+                shall at all times ensure that the User Content does not infringe any rights of third parties or any
+                applicable law. Furthermore, the User Content shall not be offensive, threatening, libelous,
+                defamatory or otherwise inappropriate. For clarity, Evolv™ is not responsible and shall not be held
+                liable for any User Content, nor does Evolv™ endorse any opinion contained in any User Content.
+                Evolv™ expressly disclaims any responsibility for the backup and/or retention of any User Content,
+                and Evolv™ Services shall not be used for back-up purposes of any User Content. Evolv™ shall have
+                the right to generate anonymous user data and statistical data from the User Content and use of the
+                User Account. Evolv™ shall reserve all rights to such anonymized data. </p>
+
+
+
+
+            <h4 class="font-display4">
+                4. INTELLECTUAL PROPERTY
+            </h4>
+
+
+
+
+            <p className=" para-display2 mb-3">Evolv™ owns and retains all intellectual property rights, including ownership of whatever IPR or
+                confidential information, including know-how and trade secrets, in Evolv™ Services. Evolv™ reserves
+                all rights not expressly granted under these Terms. Evolv™'s brand name and logo are owned by
+                Evolv™, and may not be copied, imitated or used without prior written consent of Evolv™. Nothing in
+                these Terms shall constitute a transfer of any intellectual property rights or other rights of
+                Evolv™ to the User. It is strictly forbidden for any company, organization or User to attempt to
+                host or organize any event, online or other, in conjunction with, contiguous to or purporting to be
+                related to Evolv™ without prior written consent (if any) and co-operation of Evolv™ provided at its
+                sole discretion. </p>
+
+
+
+            <h4 class="font-display4">
+                5. PERSONAL DATA
+            </h4>
+
+            <p className=" para-display2 mb-3">Evolv™ collects and processes data, including personal data, in relation to User’s registration to
+                and use of Evolv™ Services in accordance with its Privacy Policy in force from time to time. If
+                registration for the Evolv™ side event of the Business User takes place on Evolv™ platform (not
+                redirecting to other registration site), the Business User and Evolv™ shall be joint controllers for
+                the personal data processed for the purpose of the event registration. In this respect, the Business
+                User and Evolv™ agree that the data subjects shall be informed of personal data processing necessary
+                for registration, their rights and point of contact by Evolv™ Privacy Policy referred to in the
+                paragraph above. Evolv™ and the Business User shall be independently liable for taking the
+                reasonable steps to implement and maintain appropriate technical and organizational measures and
+                documentation to ensure that the processing actions they perform will meet the requirements of
+                applicable data protection regulation and ensure the protection of the rights of the data
+                subject(s). </p>
+          
+
+
+
+            <h4 class="font-display4">
+                6. CANCELLATION, POSTPONEMENT AND SUBSTITUTION POLICY
+            </h4>
+
+            <p className=" para-display2 mb-3">Evolv™ accepts no responsibility or liability whatsoever for any loss or damage of any kind incurred
+                as a result of substitution, alteration, cancellation, rescheduling or postponement of Evolv™
+                Services. In case of an unforeseen or exceptional incident, restriction or another circumstance
+                likely having an effect on Evolv™ Services, Evolv™ reserves the right to make changes to the dates
+                and time for all parts of Evolv™ Services. In case of cancellation or rescheduling any Evolv™
+                Service for a new date and/or time, Evolv™ may, at its sole discretion, transfer the service to be
+                valid on such a new date and/or time, or reimburse costs related to Evolv™ Services and paid by the
+                User to Evolv™ upon the User's request within a reasonable time limit specified by Evolv™ on a
+                case-by-case basis. After such reasonable time limit without the User having contacted Evolv™ about
+                the reimbursement, which shall be at least thirty (30) days, has expired Evolv™ will not be liable
+                for any refund or reimbursement. For the avoidance of doubt, Evolv™ reserves the right to make
+                changes to Evolv™ Services and thereto related schedules. Please note that while speakers and topics
+                may be confirmed throughout the year, circumstances beyond the control of Evolv™ may necessitate
+                substitutions, alterations or cancellations of the speakers and/or topics. As such, Evolv™ reserves
+                the right to change and/or modify the advertised time schedule and line up of speakers and/or topics
+                if necessary without any liability to the User whatsoever. Any substitutions or adjustments to the
+                foregoing will be updated on Evolv™'s channels as soon as possible. </p>
+
+
+
+            <h4 class="font-display4">
+                7. LIABILITY
+            </h4>
+
+            <p className=" para-display2 mb-3">Evolv™ will make commercially reasonable efforts to keep Evolv™ Services operational. However,
+                certain technical difficulties or maintenance may, from time to time, result in temporary
+                interruptions. Evolv™ reserves the right, periodically and at any time, to modify or discontinue,
+                temporarily or permanently, functions and features of Evolv™ Services. To the maximum extent allowed
+                by applicable laws, Evolv™ shall not be liable for any direct, indirect or consequential damages
+                arising from or relating to these Terms, Evolv™ Services, or Users participation to Evolv™
+                Conference and/or registration to Evolv™ Services, or any thereto related activity or participation.
+                However, the limitations set out herein shall not apply to damage caused by willful misconduct or
+                gross negligence. In order for the claim not to be forfeited, all claims for damage must be made
+                within thirty (30) days from the date that the damage was or should reasonably have been noticed by
+                the User. </p>
+
+
+
+
+
+
+
+
+
+            <h4 class="font-display4">
+                8. FORCE MAJEURE
+            </h4>
+
+
+
+
+            <p className=" para-display2 mb-3">Evolv™ will not be liable to the User for any breach, omission, delay or non-performance
+                of its obligations in any event of Force Majeure. Force Majeure shall mean any event mentioned below
+                or any other event, impediment or circumstance occurring within or affecting Evolv™, which:</p>
+            <ul className="mt-2 lower-roman">
+                <li className="mt-2">Is not within reasonable control of Evolv™; </li>
+                <li className="mt-2">Could not reasonably have been avoided or overcome without unreasonable expense
+                    and/or loss of time to Evolv™; </li>
+                <li className="mt-2">Is not attributable to Evolv™. If such a Force Majeure Event results in delay
+                    or non-performance of Evolv™ Services for a period of sixty (60) days or longer, then Evolv™
+                    will, notwithstanding 5 (6) W/11651606/v4 anything to the contrary, have the right to cancel
+                    Evolv™ Services without liability towards the Users. </li>
+
+            </ul>
+            <p className=" para-display2 mb-3">For the purposes of these Terms, examples of “Force Majeure Events” include, but are
+                not limited to, the following events and circumstances </p>
+
+            <ul className="mt-2 lower-roman">
+                <li className="mt-2">Any act of war (whether declared or undeclared), invasion, hostilities, armed
+                    conflict, armed attack or act of foreign enemy, blockage, embargo, revolution, riot,
+                    insurrection, civil commotion, act of terrorism or sabotage; </li>
+                <li className="mt-2">Hybrid warfare or hybrid influencing activities by state or non-state actors;
+                </li>
+                <li className="mt-2">Cyberattacks; </li>
+                <li className="mt-2">Act of authority whether lawful or unlawful, compliance with any law or
+                    governmental order, rule regulation or direction, curfew restriction, expropriation,
+                    compulsory acquisition, seizure of works nationalization; </li>
+                <li className="mt-2">Strikes or go-slows and lock outs that are widespread or nation-wide; </li>
+                <li className="mt-2">Natural events including but not limited to lightning, fire, earthquake, flood,
+                    storm, ice, cyclone, typhoon or tornado; and epidemic or other outbreaks of diseases; or
+                </li>
+                <li className="mt-2">Radioactive contamination, ionizing radiation, explosion or chemical
+                    contamination or </li>
+                <li className="mt-2">Sanctions, restrictions, prohibitions, disturbances or interruptions or other
+                    circumstances materially restricting air travel or other forms of international travel.
+                </li>
+            </ul>
+
+
+
+            <h4 class="font-display4">
+                9 . MISCELLANEOUS
+            </h4>
+
+            <p className=" para-display2 mb-3">These Terms only cover Evolv™ Services and the use thereof, and any and all linked third-party
+                services and platforms are provided by the relevant third parties and covered by their terms of use
+                or other agreement or license. Evolv™ does not assume any liability with regard to use of such
+                third-party services and platforms, whether or not they are linked to Evolv™ Services. The content
+                on Evolv™ Services, including, but not limited to, the text, graphics, images, links, and other
+                materials are for informational purposes only. Evolv™ does not recommend or endorse any specific
+                information that may be mentioned on Evolv™ Services. Reliance on any information provided on Evolv™
+                Services is solely at the User’s own risk. If any provision of these Terms is held invalid or
+                unenforceable, the remainder of these Terms shall continue in full force and effect. No agency,
+                partnership, or employment is created as a result of these Terms. </p>
+
+       
+
+
+                 
+          </div>  
+           
+     </div>
+    </section>
+    
+    </>
   )
 }
 
